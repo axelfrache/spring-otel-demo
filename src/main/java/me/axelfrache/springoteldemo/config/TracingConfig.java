4b3fa0857546fcafe828ea4f025da1f07ec254ff
Axel Frache <axelfrache@gmail.com>
2025-08-11 00:05:57 +0200

    feat: add OpenTelemetry tracing support with OTLP exporter configuration

---
+++ src/main/java/me/axelfrache/springoteldemo/config/TracingConfig.java
@@ -0,0 +1,14 @@
+package me.axelfrache.springoteldemo.config;
+
+import io.micrometer.observation.ObservationRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TracingConfig {
+
+    @Bean
+    public ObservationRegistry observationRegistry() {
+        return ObservationRegistry.create();
+    }
+}
